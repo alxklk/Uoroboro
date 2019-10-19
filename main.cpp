e9047f7fab4339741fb42893dd0180b4a4216f3c
Alex Kluchikov <klkspa@ukr.net>
2019-10-19 23:59:11 +0300

    WIP separate system-dependent header

--- main.cpp
+++ main.cpp
@@ -2,70 +2,10 @@
 
 #include "utility.h"
 #include "master.h"
-
-#ifdef __linux__
-
-#include <X11/Xlib.h>
-#include <X11/Xutil.h>
+#include "system.h"
 
 int main(int argc, char** argv)
 {
-	Display * dsp=nullptr;
-	Window win;
-	char *dspname=nullptr;
-	dsp=XOpenDisplay (dspname);
-
-	if (dsp==0)
-	{
-		fprintf(stderr, "No display available\n");
-		return 1;
-	}
-
-	Window rootwin=XDefaultRootWindow(dsp);
-	int defscreen=DefaultScreen(dsp);
-	int defdepth=DefaultDepth(dsp, defscreen);
-	int depth=defdepth;
-	Visual* visual=DefaultVisualOfScreen(DefaultScreenOfDisplay(dsp));
-
-	XVisualInfo vinfo;
-	if(XMatchVisualInfo(dsp, XDefaultScreen(dsp), 32, TrueColor, &vinfo))
-	{
-		visual = vinfo.visual;
-		depth = vinfo.depth;
-	}
-	else
-	{
-		fprintf(stderr, "No suitable visual available\n");
-		return 1;
-	}
-
-	XSetWindowAttributes attrs;
-	attrs.border_pixel=0;
-	attrs.background_pixel=0;
-	attrs.override_redirect=True;
-	attrs.colormap=XCreateColormap(dsp, XDefaultRootWindow(dsp), visual, AllocNone);
-
-	win=XCreateWindow(dsp,rootwin,0,0,1024,768,0,depth,InputOutput,visual,
-		CWBackPixel|CWBorderPixel|CWColormap,&attrs);
-
-	XRaiseWindow(dsp,win);
-
-	int EventMask=KeyPressMask|KeyReleaseMask
-								|ButtonPressMask|ButtonReleaseMask|PointerMotionMask
-								|FocusChangeMask|StructureNotifyMask
-								;
-
-	XMapWindow (dsp,win);
-	XSelectInput (dsp, win,  EventMask);
-
-	Atom WM_PROTOCOLS=0;
-	Atom WM_DELETE_WINDOW=0;
-	WM_PROTOCOLS = XInternAtom (dsp,"WM_PROTOCOLS",false);
-	WM_DELETE_WINDOW = XInternAtom (dsp,"WM_DELETE_WINDOW",false);
-	XSetWMProtocols(dsp, win, &WM_DELETE_WINDOW, 1);
-
-	XStoreName(dsp, win, "Test");
-
 	CMaster* master=new CMaster;
 	master->Init(dsp,win);
 
